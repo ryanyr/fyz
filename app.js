ced3dd144a52958a9d0c074ee2a897eb7f5a2a95
yuran <335023379@qq.com>
2017-12-22 11:10:33 +0800

    database init
    database init

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ var express = require('express');
 var path = require('path');
 var flash = require('connect-flash');
 // var config = require('./config');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var moment = require('moment');
-var mongoose = require('mongoose');
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
 
@@ -30,7 +29,8 @@ app.set('views', path.join(__dirname, './dist'));
 app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+console.log(__dirname);
+app.use(favicon(path.join(__dirname, '/dist', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -140,6 +140,7 @@ function onListening() {
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
+  console.log('Express server listening on port ' + server.address().port);
 }
 
 
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var pageController =require('../controllers/pageController');
+var userController =require('../controllers/userController');
+var postController =require('../controllers/postController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', pageController.homePage);
+router.get('/home', pageController.homePage);
 
 module.exports = router;
